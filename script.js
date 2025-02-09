64ba09be7452cc94ff0aaa0976adc94878a386f6
rohitbhardwaj24 <rn694088@gmail.com>
2025-02-09 08:07:05 +0530

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+function showLove() {
+    document.body.style.background = "url('https://source.unsplash.com/1600x900/?roses,romantic') no-repeat center center/cover";
+    document.querySelector('h1').innerText = "I Love You ❤️";
+    document.querySelector('p').innerText = "Thank you for forgiving me!";
+    document.querySelector('button').style.display = "none";
+}
